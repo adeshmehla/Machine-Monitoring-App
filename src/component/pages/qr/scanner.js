d442094f8b814926fc6c061ddabca03bc1d8cbec
Adesh <bemehla@gmail.com>
2023-02-27 22:43:53 +0530

    Add camera in app with new update

--- src/component/pages/qr/scanner.js
+++ src/component/pages/qr/scanner.js
@@ -1,7 +1,18 @@
-import React, {useEffect,useRef} from "react";
+import React, {useEffect,useRef,useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import {useSelector,useDispatch} from 'react-redux';
+import {scannerData,mechanic_name} from '../../redux/pageSlice'
 import  {Html5Qrcode} from "html5-qrcode";
 export const Scanner = (props) => {
-    const fileRef = useRef(null);
+  const [file, setfile] = useState(null);
+    const [decodedValue, setDecodedValue] = useState("");
+    // const [data, setData] = useState(null);
+    const[scanResultWebCam,setScanResultWebCam]=useState('')
+    const dispatch = useDispatch();
+    const designation_type = useSelector(state=>state.pageReducer.degination_type)
+    const navigate = useNavigate('');
+    const fileRef = useRef()
+    // const fileRef = useRef(null);
     const qrConfig = { fps: 10, qrbox: { width: 300, height: 300 } };
     let html5QrCode;
     // const [cameraList, setCameraList] = useState([]);
@@ -17,9 +28,89 @@ export const Scanner = (props) => {
       const qrCodeSuccessCallback = (decodedText, decodedResult) => {
         console.info(decodedResult, decodedText);
         props.onResult(decodedText);
-        alert(`decoded:__ ${decodedText}`);
+        // alert(`decoded:__ ${decodedText} ${decodedResult}`);
+        ////////////////////////////////////////////////////
+        fileRef.current.click();
+     
+      // const jag = ['1','2','3'];
+      // const amit = ['4','5','6'];
+      // const sunin_pal = ['7','8'];
+      // const harkesh = ['9','10','11'];
+      // const mikail = ['12','13','14'];
+      // const nafish = ['15','38'];
+      // const jitendra = ['16','17'];
+      // const vasid = ['18','19','20'];
+      // const manoj = ['21','22','23'];
+      // const ram_mahesh = ['24','25','26','27'];
+      // const vikram = ['28','29','30',];
+      // const adil = ['31','32','33',];
+      // const yoginder = ['24','35','36',];
+      // const handleScaner=async (e)=>{
+          // const file = e.target.files[0];
+          // setfile(file);
+          // const result = await QrScanner.scanImage(file);
+          if(decodedText){
+              // const b = result.split(" ")[0]
+              if(file){
+                  localStorage.setItem('qr_scannerData',decodedText)
+              }
+              // setData(result.split(""))
+              console.log(decodedText,"+++++++++++white something saying",JSON.parse(b).line_no)
+              // if(jag.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Jagvinder'))
+              // }
+              // else if(amit.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Amit'))
+              // }
+              // else if(sunin_pal.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Sunil Pal'))
+              // }
+              // else if(harkesh.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Harkesh'))
+              // }
+              // else if(mikail.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Mikail'))
+              // }
+              // else if(nafish.find(i=>i===JSON.parse(b).line_no)){
+              //     console.log('im her')
+              //     dispatch(mechanic_name('Nafish'))
+              // }
+              // else if(jitendra.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Jitendra'))
+              // }
+              // else if(vasid.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Vasid'))
+              // }
+              // else if(manoj.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Manoj'))
+              // }
+              // else if(ram_mahesh.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Ram Mahesh'))
+              // }
+              // else if(vikram.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Vikram'))
+              // }
+              // else if(adil.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Adil'))
+              // }
+              // else if(yoginder.find(i=>i===JSON.parse(b).line_no)){
+              //     dispatch(mechanic_name('Yoginder'))
+              // }
+              // else{
+              //     console.log('i am in else condition000000000000')
+              // }
+              dispatch(scannerData(decodedText));
+              console.log(designation_type,'innn qrcode page')
+              if(designation_type==="Machanic"){
+                  // navigate('/machanic')
+                  navigate("/mechanic", { replace: true });
+              }else{
+                  navigate('/supervisor')
+              }
+  
+          }
+        };
         handleStop();
-      };
       html5QrCode
         .start(
           { facingMode: "environment" },
